3ab75a2d9e384d2b43dab140822c789d3bcfb39a
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-04-04 15:12:37 +0100

    feat: adição de um video

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import video2 from './medias/Grace Zola - Deus de Promessas (Official Music Vide
 import video3 from './medias/Rosny Kayiba - Mon meilleur ami ( Clip officiel).mp4';
 import video4 from './medias/Grace Zola - Oza Monene feat. Ruth Kuniasa (Official Music Video).mp4';
 import video5 from './medias/Rhema Loseke - Yaya ( Clip Officiel).mp4';
+import video6 from './medias/Eva Rapdiva x Deezy - Incondicional _ Prod Teo No Beat.mp4';
 
 // Importando as imagens das capas
 import capa1 from './medias/Capa1.jpg';
@@ -15,6 +16,7 @@ import capa2 from './medias/Capa2.jpg';
 import capa3 from './medias/Capa3.jpg';
 import capa4 from './medias/Capa4.jpg';
 import capa5 from './medias/Capa5.jpg';
+import capa6 from './medias/Capa6.jpg';
 
 // Lista de vídeos
 const videoList = [
@@ -22,7 +24,8 @@ const videoList = [
   { id: 2, title: 'Grace Zola - Deus de Promessas', url: video2, capa: capa2 },
   { id: 3, title: 'Rosny Kayiba - Mon meilleur ami', url: video3, capa: capa3 },
   { id: 4, title: 'Grace Zola - Oza Monene feat. Ruth Kuniasa', url: video4, capa: capa4 },
-  { id: 5, title: 'Rhema Loseke - Yaya', url: video5, capa: capa5 }
+  { id: 5, title: 'Rhema Loseke - Yaya', url: video5, capa: capa5 },
+  { id: 6, title: 'Eva Rapdiva x Deezy - Incondicional', url: video6, capa: capa6 }
 ];
 
 function App() {
